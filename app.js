e289ea53878ded3120176bf69262d500c3541c8a
adeogunesther95-lab <adeogunesther95@gmail.com>
2025-09-28 17:59:04 +0100

    removed JS for correction

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = `${cityInputElement.value}`;
-}
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSubmit);
